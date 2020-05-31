dae1fe610084ab7bdaf44493b6b28fb544b88158
Ivan Aguas <aguas.xavier2013a@gmail.com>
2020-05-30 23:22:24 -0500

    Add One hot encoder

--- feedForwardNN.py
+++ feedForwardNN.py
@@ -14,12 +14,15 @@ import pandas as pd
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
+y = dataset.iloc[:, 13]
 
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
+X[:, 2] = labelencoder_X_1.fit_transform(X[:, 2])
+
+ct = ColumnTransformer([("Geography", OneHotEncoder(), [0])], remainder = 'passthrough')
+X = ct.fit_transform(X)
