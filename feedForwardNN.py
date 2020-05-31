60d50062649e4284759611d96decb9df8af534d3
Ivan Aguas <aguas.xavier2013a@gmail.com>
2020-05-31 10:19:01 -0500

    Add plot confusion matrix

--- feedForwardNN.py
+++ feedForwardNN.py
@@ -11,10 +11,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13]
+y = dataset.iloc[:, 13].values 
 
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -24,5 +28,48 @@ X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_1.fit_transform(X[:, 2])
 
-ct = ColumnTransformer([("Geography", OneHotEncoder(), [0])], remainder = 'passthrough')
-X = ct.fit_transform(X)
+
+# ct = ColumnTransformer([("Geography", OneHotEncoder(), [0])], remainder = 'passthrough')
+
+# X = ct.fit_transform(X).toarray()
+# X = X[:, 1:]
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+classifier = Sequential()
+
+classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu', input_dim = 10))
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['mse'])
+
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+
+
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# from sklearn.metrics import confusion_matrix
+# cm = confusion_matrix(y_test, y_pred)
+
+np.set_printoptions(precision=2)
+from sklearn.metrics import plot_confusion_matrix
+
+disp = plot_confusion_matrix(classifier, X_test, y_test, display_labels=['Yes','No'], cmap=plt.cm.Blues,normalize=none)
+
+
+plt.show()
+
+
